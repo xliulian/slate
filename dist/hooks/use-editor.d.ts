58e245664f9e917593d7ae6bc496ea972c975b8f
Ulion <ulion2002@gmail.com>
2021-01-05 07:51:24 +0800

    gitpkg

---
+++ dist/hooks/use-editor.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="react" />
+import { ReactEditor } from '../plugin/react-editor';
+/**
+ * A React context for sharing the editor object.
+ * @deprecated Use useSlateStatic instead.
+ */
+export declare const EditorContext: import("react").Context<ReactEditor | null>;
+/**
+ * Get the current editor object from the React context.
+ */
+export declare const useEditor: () => ReactEditor;
+//# sourceMappingURL=use-editor.d.ts.map
