bbfe75ee972c173bf28b98536bb39d2ed460d9a8
Ulion <ulion2002@gmail.com>
2021-01-05 09:06:13 +0800

    gitpkg

---
+++ dist/plugin/with-react.d.ts
@@ -0,0 +1,6 @@
+import { ReactEditor } from './react-editor';
+/**
+ * `withReact` adds React and DOM specific behaviors to the editor.
+ */
+export declare const withReact: <T extends import("slate").BaseEditor>(editor: T) => T & ReactEditor;
+//# sourceMappingURL=with-react.d.ts.map
