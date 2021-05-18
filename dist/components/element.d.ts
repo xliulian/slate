667bdfc469bc572b097b3303a41154fa3fa88745
Ulion <ulion2002@gmail.com>
2021-05-18 13:42:39 +0800

    gitpkg

---
+++ dist/components/element.d.ts
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Range, Element as SlateElement } from 'slate';
+import { RenderElementProps, RenderLeafProps } from './editable';
+/**
+ * Element.
+ */
+declare const Element: (props: {
+    decorations: Range[];
+    element: SlateElement;
+    renderElement?: ((props: RenderElementProps) => JSX.Element) | undefined;
+    renderLeaf?: ((props: RenderLeafProps) => JSX.Element) | undefined;
+    selection: Range | null;
+}) => JSX.Element;
+declare const MemoizedElement: React.MemoExoticComponent<(props: {
+    decorations: Range[];
+    element: SlateElement;
+    renderElement?: ((props: RenderElementProps) => JSX.Element) | undefined;
+    renderLeaf?: ((props: RenderLeafProps) => JSX.Element) | undefined;
+    selection: Range | null;
+}) => JSX.Element>;
+/**
+ * The default element renderer.
+ */
+export declare const DefaultElement: (props: RenderElementProps) => JSX.Element;
+export default MemoizedElement;
+//# sourceMappingURL=element.d.ts.map
