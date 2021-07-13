74357f74108cbbd3c72322fd1de8a9019ca81caf
Ulion <ulion2002@gmail.com>
2021-07-13 21:04:48 +0800

    gitpkg

---
+++ dist/hooks/use-index.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="react" />
+/**
+ * A React context for sharing the `selected` state of an element.
+ */
+export declare const IndexContext: import("react").Context<number>;
+/**
+ * Get the current `selected` state of an element.
+ */
+export declare const useIndex: () => number;
+//# sourceMappingURL=use-index.d.ts.map
